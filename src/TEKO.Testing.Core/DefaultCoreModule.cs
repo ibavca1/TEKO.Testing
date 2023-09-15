249fa0ba0b4b5ba4165cae187597c38aac76e826
ibavc <ibavca@gmail.com>
2023-09-15 07:28:39 +0500

    Test

--- src/TEKO.Testing.Core/DefaultCoreModule.cs
+++ src/TEKO.Testing.Core/DefaultCoreModule.cs
@@ -5,9 +5,6 @@ using TEKO.Testing.Core.Services;
 
 namespace TEKO.Testing.Core;
 
-/// <summary>
-/// An Autofac module that is responsible for wiring up services defined in the Core project.
-/// </summary>
 public class DefaultCoreModule : Module
 {
   protected override void Load(ContainerBuilder builder)
